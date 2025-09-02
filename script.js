b12634eb107f0ad3b7af013a835c4197efcace6a
Jenny Porada <jennyingles@gmail.com>
2025-09-02 14:35:51 -0400

    script

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
 document.head.insertAdjacentHTML(
     'beforeend',
     '<link rel="stylesheet" href="https://breakdancingcat.github.io/css/styles.css" />');
-</script>
