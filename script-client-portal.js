8fa08edcdc4530c5839b25de08317714fa53a385
Jenny Porada <jennyingles@gmail.com>
2025-09-02 17:47:23 -0400

    styles

--- script-client-portal.js
+++ script-client-portal.js
@@ -1,2 +1,7 @@
+document.head.insertAdjacentHTML(
+    'beforeend',
+    '<link rel="stylesheet" href="https://breakdancingcat.github.io/css/styles-client-portal.css" />',
+);
+
 document.body.classList.add('wsu-client-portal');
 
